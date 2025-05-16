908ba99c7759156c7f94689a98d6f7c026dae749
Kandi <140391941+Andrijjan@users.noreply.github.com>
2025-05-16 14:28:29 +0200

    Create functions.h

---
+++ functions.h
@@ -0,0 +1,16 @@
+#ifndef functions
+#define functions
+
+#include "datatype.h"
+
+void clearConsole();
+int mainMenu();
+int pjesmaMENU();
+void createFile();
+void readPJESME(int* noFilms, PJESMA** arrayFilms);
+void addPJESMA();
+void showAllPJESMA(short unsigned enter);
+void deletePJESMA();
+void updatePJESMA();
+
+#endif#pragma once
