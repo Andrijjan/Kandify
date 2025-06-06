99d9d9fe1efd41e7d57906f0d67268280a1c7589
Kandi <140391941+Andrijjan@users.noreply.github.com>
2025-06-06 10:03:30 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include "kandify.h"
 
+char trenutna_datoteka[256];  // Definicija globalne varijable
+
 int main() {
+    // Učitaj naziv datoteke iz konfiguracije, ako nema postavi default
+    if (!ucitajNazivDatoteke()) {
+        strcpy(trenutna_datoteka, "datoteka.bin");
+    }
+
     prikaziGlavniMeni();
     return 0;
 }
