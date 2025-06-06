fe826fa2de3d6458860322147fbc901fac6b4d8f
Kandi <140391941+Andrijjan@users.noreply.github.com>
2025-06-06 08:28:58 +0200

    Update kandify.h

--- kandify.h
+++ kandify.h
@@ -1,6 +1,8 @@
 #ifndef KANDIFY_H
 #define KANDIFY_H
 
+#include <stdio.h>
+
 #define MAX_NAZIV 100
 #define MAX_AUTOR 100
 #define MAX_ZANR 50
@@ -16,12 +18,29 @@ typedef struct {
     float trajanje;
 } Zapis;
 
+typedef enum {
+    IZLAZ = 0,
+    MENI_PJESAMA = 1
+} GlavniMeniOpcije;
+
+typedef enum {
+    POVRATAK = 0,
+    DODAJ_ZAPIS = 1,
+    PRIKAZI_ZAPISE = 2,
+    UREDI_ZAPIS = 3,
+    IZBRISI_ZAPIS = 4
+} MeniPjesamaOpcije;
+
 void prikaziGlavniMeni();
 void prikaziMeniPjesama();
 void dodajZapis();
 void prikaziZapise();
 void urediZapis();
 void izbrisiZapis();
-int ucitajZapise(Zapis zapisi[], int max);
+
+static inline void ocistiBuffer() {
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF);
+}
 
 #endif
