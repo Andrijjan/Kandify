54a35a7b5b94a3790c5d9a913b123237e9350639
Kandi <140391941+Andrijjan@users.noreply.github.com>
2025-06-16 23:46:40 +0200

    Update menipjesama.c

--- menipjesama.c
+++ menipjesama.c
@@ -15,9 +15,10 @@ void prikaziMeniPjesama() {
         printf("6. Pretrazi zapis\n");
         printf("7. Obrisi datoteku\n");
         printf("8. Preimenuj datoteku\n");
+        printf("9. Kopiraj datoteku\n");
         printf("0. Povratak na glavni meni\n");
         printf("-----------------------------------\n");
-        printf("\nUnesite izbor: ");
+        printf("Unesite izbor: ");
 
         if (scanf("%d", (int*)&izbor) != 1) {
             printf("Pogresan unos.\n");
@@ -26,16 +27,17 @@ void prikaziMeniPjesama() {
         }
 
         switch (izbor) {
-        case DODAJ_ZAPIS: dodajZapis(); break;
-        case PRIKAZI_ZAPISE: prikaziZapise(); break;
-        case UREDI_ZAPIS: urediZapis(); break;
-        case IZBRISI_ZAPIS: izbrisiZapis(); break;
-        case SORTIRAJ_ZAPISE: sortirajZapise(); break;
-        case PRETRAZI_ZAPIS: pretraziZapis(); break;
-        case BRISI_DATOTEKU: obrisiDatoteku(); break;
+        case DODAJ_ZAPIS:        dodajZapis();        break;
+        case PRIKAZI_ZAPISE:     prikaziZapise();     break;
+        case UREDI_ZAPIS:        urediZapis();        break;
+        case IZBRISI_ZAPIS:      izbrisiZapis();      break;
+        case SORTIRAJ_ZAPISE:    sortirajZapise();    break;
+        case PRETRAZI_ZAPIS:     pretraziZapis();     break;
+        case BRISI_DATOTEKU:     obrisiDatoteku();    break;
         case PREIMENUJ_DATOTEKU: preimenujDatoteku(); break;
-        case POVRATAK: break;
-        default: printf("Pogresan unos.\n"); break;
+        case KOPIRAJ_DATOTEKU:   kopirajDatoteku();   break;
+        case POVRATAK:           break;
+        default:                 printf("Pogresan unos.\n"); break;
         }
     } while (izbor != POVRATAK);
 }
